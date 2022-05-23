4db141acdbcaf1cf3976168964963872a8a8e75e
hicham <hzaydi24@gmail.com>
2022-05-23 22:09:23 +0100

    NavBar added

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <title>Projet PFA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>{' '}
-      <NavBar />
+      {/* <NavBar /> */}
       {/* <Questions /> */}
       <Login />
     </div>
