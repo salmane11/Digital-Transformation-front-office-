e80ba696221b51baddaf33adfedfdd447fdd396c
salmane <salmanchafik@gmail.com>
2022-06-14 00:07:33 +0100

    final touches

--- components/results/ResultTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ResultTitle({ title }) {
-  return (
-    <div className="ml-8 p-4">
-      <h1 className="text-xl font-semibold">Résultats de l'audit {title}</h1>
-    </div>
-  )
-}
